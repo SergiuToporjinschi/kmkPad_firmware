7c19bf465b088204354c3b2a4f171e0888f1172f
SergiuToporjinschi <Sergiu.Toporjinschi@gmail.com>
2024-11-05 10:44:07 +0200

    temp 1

---
+++ custom/desktop_app/config.py
@@ -0,0 +1,120 @@
+import json
+from kmk.keys import KC
+from kmk.utils import Debug
+
+debug = Debug(__name__)
+
+config = None
+
+class ConfigHandler:
+    _instance = None
+    _keyboard_layers = None
+    _nr_keyboard_layers = None
+    _encoder_layers = None  
+
+    
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(ConfigHandler, cls).__new__(cls)
+        
+        global config
+        config = cls._instance
+        return cls._instance
+
+    def __init__(self, no_of_keys: int = None):
+        if not hasattr(self, 'initialized'):
+            self._no_of_keys = no_of_keys
+            self.initialized = True
+    
+    def load_config(self):
+        self.config = self._read_config()
+        if debug.enabled:
+            debug('Config loaded')
+
+    def _read_config(self):
+        with open('/config.json') as f:
+            return json.load(f)
+
+    @property
+    def screen_brightness(self) -> float:
+        return self.config['screen']['brightness']
+
+    @property
+    def screen_brightness_step(self) -> float:
+        return self.config['screen']['brightnessStep']
+
+    @property
+    def screen_flip(self) -> bool:
+        return self.config['screen']['flip']
+    
+    @property
+    def screen_dim_timeout(self) -> int:
+        return self.config['screen']['dimTimeout']
+    
+    @property
+    def screen_dim_target(self) -> float:
+        return self.config['screen']['dimTarget']
+
+    @property
+    def screen_off_time(self) -> int:
+        return self.config['screen']['offTime']
+    
+    @property
+    def encoder_reversed(self) -> bool:
+        return self.config['encoders'][0]['reversed']
+    
+    @property
+    def encoder_divisor(self) -> int:
+        return self.config['encoders'][0]['divisor']
+
+    def layers_get_by_index(self, index: int) -> dict:
+        for layer in self.config['keyboard']['layers']:
+            if layer['index'] == index:
+                return layer
+        return None
+
+    @property
+    def layers_key_maps(self) -> list:
+        if self._keyboard_layers is not None:
+            return self._keyboard_layers
+        
+        ret_layers = [None] * len(self.config['keyboard']['layers'])
+        for layer in self.config['keyboard']['layers']:
+            ret_layers[layer['index']] = self._build_key_map(layer['map'], self._no_of_keys)
+
+        self._keyboard_layers = ret_layers
+        self._nr_keyboard_layers = len(ret_layers)
+        return ret_layers
+    
+    @property
+    def layers_encoders_maps(self) -> list:
+        if self._encoder_layers is not None:
+            return self._encoder_layers
+        
+        # for encoder in self.config['encoders']:
+        enc_layers =  [None] * self._nr_keyboard_layers
+        for i in range(self._nr_keyboard_layers):
+            layer = self._find_layer_by_index(self.config['encoders'][0]['layers'], i)
+            if layer is not None:
+                enc_layers[i] = [self._build_key_map(layer['map'], 3)]
+            else:
+                enc_layers[i] = [[KC.TRNS, KC.TRNS, KC.TRNS]]
+        
+        self._encoder_layers = enc_layers
+        debug('Encoder layers:', enc_layers)
+        return enc_layers
+    # [[[]],[[]]]
+    def _build_key_map(self, key_map: list, no_of_keys: int): #TODO: add support for parameter keys
+        ret_key_map = [] 
+        for i in range(no_of_keys):
+            if i < len(key_map):
+                ret_key_map.append(KC.get(key_map[i], "NO"))
+            else:
+                ret_key_map.append(KC.NO)
+        return ret_key_map
+    
+    def _find_layer_by_index(self, layers: list, index: int) -> dict:
+        for layer in layers:
+            if layer['index'] == index:
+                return layer
+        return None
--- custom/screen/screen.py
+++ custom/screen/screen.py
@@ -1,12 +1,13 @@
-from kmk.keys import make_key
+from kmk.keys import make_key,make_argumented_key, Key
 from kmk.utils import Debug
 from kmk.extensions.display import Display
 from adafruit_display_text import label
 from adafruit_display_shapes.line import Line
 from adafruit_bitmap_font import bitmap_font
-from custom.desktop_app.layer_descriptor import readLayerDescriptor
+# from custom.desktop_app.layer_descriptor import readLayerDescriptor
 import terminalio
 import displayio
+from custom.desktop_app.config import ConfigHandler
 
 debug = Debug(__name__)
 
@@ -21,46 +22,53 @@ class Font:
             return bitmap_font.load_font(font)
         return terminalio.FONT
     
-# class ScreenKey(Key):
-#     def __init__(self, line, message, key=None, **kwargs):
-#         super().__init__(**kwargs)
-#         self.line = line
-#         self.message = message
-#         self.key = key
+class ScreenKey(Key):
+    def __init__(self, line, message,  **kwargs):
+        super().__init__(**kwargs)
+        self.line = line
+        self.message = message
 
 class Screen(Display):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         debug("Screen extension initialized")
         self._lastLayer = None
         self._create_keys()
         self._bild_screen()
-        self._layer_descriptor = readLayerDescriptor()
+        self._config = ConfigHandler()
+        # self._layer_descriptor = readLayerDescriptor()
 
     def _create_keys(self):
-        make_key(('SK_INF1',),
-            on_press=self.on_screen_info_line1_press,
-            on_release=self.on_screen_info_line1_release,
-        )
-        make_key(('SK_INF2',),
-            on_press=self.on_screen_info_line2_press,
-            on_release=self.on_screen_info_line2_release,
+        make_argumented_key(
+            names=('SK_INF',),
+            constructor=ScreenKey,
+            on_press=self.on_screen_info_line_press,
+            on_release=self.on_screen_info_line_release,
         )
 
-    def on_screen_info_line1_press(self, key, keyboard): pass
-    def on_screen_info_line1_release(self, key, keyboard): pass
-    def on_screen_info_line2_press(self, key, keyboard): pass
-    def on_screen_info_line2_release(self, key, keyboard): pass
+    def on_screen_info_line_press(self, key, keyboard, *args, **kwargs): 
+        if key.line == 1:
+            self._info_line_1.text = key.message
+        if key.line == 2:
+            self._info_line_2.text = key.message
+
+    def on_screen_info_line_release(self, key, keyboard, *args, **kwargs): 
+        if key.line == 1:
+            self._info_line_1.text = ""
+        if key.line == 2:
+            self._info_line_2.text = ""
+
 
     def _bild_screen(self):
         self._name_title = label.Label(Font.GetFont(Font.ArialBold15), text="DEV Pad", color=0xFFFFFF, x=0, y=5, anchor_point=(0.0, 0.0)) #TODO get value from config 
         self._name_title_version = label.Label(Font.GetFont(Font.ArialBold8), text="V 0.01", color=0xFFFFFF, x=0, y=24, anchor_point=(0.0, 0.0)) #TODO get value from config
         self._layer_label = label.Label(Font.GetFont(Font.ArialBold15), text="Layer", color=0xFFFFFF, x=75, y=5, anchor_point=(0.0, 0.0))
-        
         self._layer_value = label.Label(Font.GetFont(Font.ArialBold10), text=" "*7, color=0xFFFFFF, x=75, y=24, anchor_point=(0.0, 0.0))
         
+        self._info_line_1 = label.Label(Font.GetFont(Font.ArialBold10), text=" "*20, color=0xFFFFFF, x=0, y=44, anchor_point=(0.0, 0.0))
+        self._info_line_2 = label.Label(Font.GetFont(Font.ArialBold10), text=" "*20, color=0xFFFFFF, x=0, y=56, anchor_point=(0.0, 0.0))
+
         self._screen_group = displayio.Group()
         self._screen_group.append(self._name_title)
         self._screen_group.append(self._name_title_version)
@@ -68,6 +76,9 @@ class Screen(Display):
         self._screen_group.append(self._layer_label)
         self._screen_group.append(self._layer_value)
         self._screen_group.append(Line(0, 32, 128, 32, 0xFFFFFF))
+        self._screen_group.append(self._info_line_1)
+        self._screen_group.append(self._info_line_2)
+        
 
     def on_runtime_enable(self, keyboard):
         super().on_runtime_enable(keyboard)
@@ -82,16 +93,13 @@ class Screen(Display):
 
     def before_matrix_scan(self, keyboard):
         super().before_matrix_scan(keyboard)
-        debug
-        if self.display is not None and self.display.root_group != self._screen_group:
-            self.display.root_group = self._screen_group
+        # if self.display is not None and self.display.root_group != self._screen_group:
+        #     self.display.root_group = self._screen_group
         
-        if self._current_layer != keyboard.active_layers[0]:
-            self._current_layer = keyboard.active_layers[0]
-            for layer in self._layer_descriptor:
-                if layer['Index'] == keyboard.active_layers[0]:
-                    self._layer_value.text = layer['Name']
-                    break
+        # if self._current_layer != keyboard.active_layers[0]:
+        #     self._current_layer = keyboard.active_layers[0]
+        #     layer_config = self._config.layers_get_by_index(self._current_layer)
+        #     self._layer_value.text = layer_config['name']
 
     def after_matrix_scan(self, keyboard): super().after_matrix_scan(keyboard)
     def before_hid_send(self, keyboard): pass
